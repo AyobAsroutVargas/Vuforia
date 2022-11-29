860597a582686ef33ffe11018226b2e049ce6e12
Ayob Asrout Vargas <alu0101350158@ull.edu.es>
2022-11-29 04:00:13 +0000

    Add files via upload

---
+++ Events.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+  public GameObject Astronaut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeColor()
+    {
+      Astronaut.GetComponent<Renderer>().material.color = Random.ColorHSV();
+    Debug.Log("Cambiando color");
+    }
+}
